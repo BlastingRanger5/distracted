b122a7633be5ed4b70350342e55d37415846959a
Cam <blastingranger5@gmail.com>
2021-03-07 20:38:12 -0500

    Implemented working button using Debug Log

---
+++ B Cool Studios - Prototype #1/Assets/Scripts/CamButtonScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CamButtonScript : MonoBehaviour
+{
+
+    public Button answer1;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ButtonPressed()
+    {
+        Debug.Log("You've pressed the button");
+        StartCoroutine(postButton());
+    }
+
+
+    IEnumerator postButton()
+    {
+        yield return new WaitForSeconds(30f);
+        Debug.Log("Y E S");
+    }
+
+
+}
