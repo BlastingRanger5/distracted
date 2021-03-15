b737d47d4a0fcc1b00a5707b798f8ccb55f28708
Daniel Karlsrud Berg <daniel.karlsrud.berg@gmail.com>
2021-03-15 04:23:52 -0400

    I finished my part of the game (and more)
    I have done the spawning of cars, car controller, modeled a prefab car, made road spawn in front of the car, made an end screen if you crash into another car, imported sounds. It is now 4:23am, I am going to bed.

---
+++ B Cool Studios - Prototype #2/Assets/Scripts/AICarController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AICarController : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 move;
+
+    public float forwardMotion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move = new Vector3(0, 0, forwardMotion);
+
+        controller.Move(move * Time.deltaTime);
+    }
+}
--- B Cool Studios - Prototype #2/Assets/Scripts/CarController.cs
+++ B Cool Studios - Prototype #2/Assets/Scripts/CarController.cs
@@ -7,27 +7,72 @@ public class CarController : MonoBehaviour
     private float horizontalMove = 0f;
     private CharacterController controller;
     private Vector3 move;
+    private Vector3 rotation;
     
     public float veeringSpeed;
+    public float forwardMotion;
     public Joystick joystick;
+    public GameObject steeringWheel;
+    public GameObject blackScreen;
+    public bool gameRunning;
+    public GameObject PSA;
+
 
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
+
+        gameRunning = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         horizontalMove = joystick.Horizontal * veeringSpeed;
-        move = new Vector3(horizontalMove, 0, 0);
-
-        if(horizontalMove != 0)
+        if(gameRunning)
         {
-            controller.Move(move * veeringSpeed * Time.deltaTime);
+            move = new Vector3(horizontalMove * veeringSpeed, 0, forwardMotion);
+        }
+        else
+        {
+            move = new Vector3(0, 0, 0);
         }
 
-        Debug.Log(horizontalMove);
+        rotation = new Vector3(0, horizontalMove, 0);
+
+        controller.Move(move * Time.deltaTime);
+        
+        steeringWheel.transform.Rotate(rotation);
+
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Hazard"))
+        {
+            blackScreen.SetActive(true);
+
+            gameRunning = false;
+
+            StartCoroutine(SoundLink());
+
+            Debug.Log("Game Over");
+        }
+    }
+
+    private IEnumerator SoundLink()
+    {
+        FindObjectOfType<AudioManager>().Play("Scream");
+
+        yield return new WaitForSeconds(2);
+
+        FindObjectOfType<AudioManager>().Play("Crash");
+
+        yield return new WaitForSeconds(1);
+
+        PSA.SetActive(true);
     }
 }
