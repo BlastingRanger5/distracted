472e925dc44a37d4ebc44d9fbb47608837eefcec
Daniel Karlsrud Berg <daniel.karlsrud.berg@gmail.com>
2021-03-14 23:00:39 -0400

    Added Joystick Package and made a car prefab

---
+++ B Cool Studios - Prototype #2/Assets/Scripts/CarController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    private float horizontalMove = 0f;
+    
+    public float veeringSpeed;
+    public Joystick joystick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = joystick.Horizontal * veeringSpeed;
+
+        Debug.Log(horizontalMove);
+    }
+}
